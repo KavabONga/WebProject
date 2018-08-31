e3d22f558a63521e5aa280c677ee13d5c9591267
Mitya Shabat <mitya.shabat@gmail.com>
2018-08-31 11:14:11 +0300

    Minor corrections

--- Scripts/searchers.py
+++ Scripts/searchers.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+def ffilter(f, ar):
+    return list(filter(f, ar))
+def fmap(f, ar):
+    return list(map(f, ar))
+
 class TermLink:
     def __init__(self, words, link):
         self.words = words
@@ -13,9 +18,14 @@ class TermLink:
 
 class Searcher:
     mainApiPage = ""
+
+    # Returns only the singular-word terms
+    # (Maybe sometime I'll be able to highlight double-word terms, but i'd need ML for that)
     @staticmethod
     def split_words(line):
-        return list(map(lambda x: x.strip(' '), re.split('[\.,]', line)))
+        return ffilter(lambda word: ' ' not in word, fmap(lambda x: x.strip(' '), re.split('[\.,]', line)))
+
+    # Returns list of TermLinks extracted from mainApiPage
     @classmethod
     def get_term_links(cls):
         pass
@@ -28,40 +38,31 @@ class BiologySearcher(Searcher):
     def get_term_links(cls):
         soup = BeautifulSoup(requests.get(cls.mainApiPage).text, 'html.parser')
         a_tags = soup.findAll('a')
-        right_tags = list(
-            filter(
-                lambda s: s.text.capitalize() == s.text and
-                s.get('href') is not None and
-                s.get('href').startswith(
-                    '/free/6-biologiya/25-slovar_biologicheskih_terminov/stages'),
-                a_tags
-            )
+        right_tags = ffilter(
+            lambda s: s.text.capitalize() == s.text and
+            s.get('href') is not None and
+            s.get('href').startswith(
+                '/free/6-biologiya/25-slovar_biologicheskih_terminov/stages'),
+            a_tags
         )
-        return list(
-            map(
-                lambda a: TermLink(a.text.split(','), a.get('href')),
-                right_tags
-            )
+        return fmap
+            lambda a: TermLink(split_words(a.text), a.get('href')),
+            right_tags
         )
 
 
 class GeographySearcher(Searcher):
     mainApiPage = "http://www.ecosystema.ru/07referats/slovgeo/index.htm"
-
+    @classmethod
     def get_term_links(cls):
         soup = BeautifulSoup(requests.get(mainApiPage).text, 'html.parser')
         a_tags = soup.findAll("a")
-        right_tags = list(
-            filter(
-                lambda s: s.get('href') is not None and bool(re.match(r'\d+\.htm', s.get('href'))),
-                a_tags
-            )
+        right_tags = ffilter(
+            lambda s: s.get('href') is not None and bool(
+                re.match(r'\d+\.htm', s.get('href'))),
+            a_tags
         )
-        return list(
-            map(
-                lambda s: TermLink(
-                    re.split(r'[\.,]', s.text),
-                    s.get('href')
-                )
-            )
+        return fmap(
+            lambda s: TermLink(split_words(s.text), "http://www.ecosystema.ru/07referats/slovgeo/" + s.get('href')),
+            right_tags
         )
